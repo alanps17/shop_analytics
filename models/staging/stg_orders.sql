2b15b88f66af98f46fe2435d02132e1c64bf7082
alanps17 <138964090+alanps17@users.noreply.github.com>
2026-09-03 22:01:14 +0300

    Staging слой на данных Olist

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,8 +1,10 @@
 select
     order_id,
     customer_id,
-    product_id,
-    quantity,
-    status,
-    order_date
-from shop_analytics.orders
+    order_status,
+    toDateTimeOrNull(order_purchase_timestamp) as order_purchase_ts,
+    toDateTimeOrNull(order_approved_at) as order_approved_ts,
+    toDateTimeOrNull(order_delivered_carrier_date) as order_delivered_carrier_ts,
+    toDateTimeOrNull(order_delivered_customer_date) as order_delivered_customer_ts,
+    toDateTimeOrNull(order_estimated_delivery_date) as order_estimated_delivery_ts
+from shop_analytics.orders_raw
